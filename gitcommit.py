1e0eac595ecc574c6d5813e53d95f3eb3160b91d
timrsj <timrsj@gmail.com>
2020-04-13 10:59:31 +1000

    update

--- gitcommit.py
+++ gitcommit.py
@@ -1,7 +1,7 @@
 import subprocess as cmd 
 
 message = 'update '
-cp = cmd.run("git add *.*", check=True, shell=True)
+cp = cmd.run("git add .", check=True, shell=True)
 print(cp)
 cp = cmd.run(f"git commit -m {message}", check=True, shell=True)
 print(cp)
